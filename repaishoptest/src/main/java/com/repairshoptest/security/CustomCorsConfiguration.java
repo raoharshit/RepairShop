f624784bf921e2678e38a633fe60b7e28b18d9e0
ansal <ansaljain7@gmail.com>
2024-11-04 12:48:20 +0530

    cors fix

--- repaishoptest/src/main/java/com/repairshoptest/security/CustomCorsConfiguration.java
+++ repaishoptest/src/main/java/com/repairshoptest/security/CustomCorsConfiguration.java
@@ -16,6 +16,7 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
 		config.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:8080"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
 		return config;
 	}
 }
